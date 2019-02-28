7aafa6f5f83e86727d7da2e4e5e8c317957bad25
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2019-02-28 12:45:19 +0000

    Initial Commit v2 - First one unsuccessful

---
+++ script.js
@@ -0,0 +1,41 @@
+function mobileMenuToggle(){
+	let hamburgerMenu = document.getElementById('hamburger-menu');
+	let mobileMenu = document.querySelector('nav');
+		hamburgerMenu.addEventListener('click', function(){
+			if (hamburgerMenu.classList.contains('is-active')){
+				hamburgerMenu.classList.remove('is-active');
+				mobileMenu.style.width = 0;
+
+			} else {
+				hamburgerMenu.classList.add('is-active');
+				mobileMenu.style.width = '300px';
+			}
+		});
+};
+
+function setWelcomeSectionHeight(){
+	let headerHeight = document.getElementById('header').clientHeight;
+	document.getElementById('section-welcome').style.height = (window.innerHeight - headerHeight) + "px";
+}
+
+
+window.addEventListener('scroll', function() {
+	let triggeredCard, triggeredServices;
+    if(window.scrollY > 200 && triggeredCard !== true) {
+        Array.from(document.querySelectorAll(".card"), e => e.style.opacity = "1");
+        triggeredCard = true;
+    }
+
+    if(window.scrollY > 900 && triggeredServices !== true) {
+    	document.querySelector(".skills-list").style.opacity = "1";
+    	triggeredServices = true;
+    }
+});
+
+
+
+setWelcomeSectionHeight();
+mobileMenuToggle();
+
+
+
