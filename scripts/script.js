0408f37e2b13d7c04842bac1cbf7d03e71ace8b8
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2019-03-20 17:46:04 +0000

    added testimonial slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+
+// ----- HAMBURGER MENU ----- //
 function mobileMenuToggle(){
 	let hamburgerMenu = document.getElementById('hamburger-menu');
 	let mobileMenu = document.querySelector('nav');
@@ -8,18 +10,32 @@ function mobileMenuToggle(){
 
 			} else {
 				hamburgerMenu.classList.add('is-active');
-				mobileMenu.style.width = '300px';
+				mobileMenu.style.width = '200px';
 			}
 		});
 };
+mobileMenuToggle();
 
+// ----- UPDATING THE SINGULAR IMAGE IN THE LIST OF SERVICES ----- //
+document.querySelector('#plastering-img-container').addEventListener("mouseover", (e) =>{
+	document.getElementById('plaster-img').src = "icons/plasteringblack.png";
+	console.log(e.target);
+});
+
+document.querySelector('#plastering-img-container').addEventListener("mouseleave", (e) =>{
+	document.getElementById('plaster-img').src = "icons/plasteringwhite.png";
+	console.log(e.target);
+});
+
+
+// ----- DYNAMICALLY SET THE WELCOME SECTION HEIGHT BASED ON HEADER HEIGHT ----- //
 function setWelcomeSectionHeight(){
 	let headerHeight = document.getElementById('header').clientHeight;
 	document.getElementById('section-welcome').style.height = (window.innerHeight - headerHeight) + "px";
-
 }
+setWelcomeSectionHeight();
 
-
+// ----- SCROLL FUNCTION TO MAKE CONTENT DYNAMICALLY APPEAR ----- //
 window.addEventListener('scroll', function() {
 	let triggeredCard, triggeredServices;
     if(window.scrollY > 200 && triggeredCard !== true) {
@@ -33,12 +49,16 @@ window.addEventListener('scroll', function() {
     }
 });
 
-
-setWelcomeSectionHeight();
-mobileMenuToggle();
-
+// ----- INITIALISE MATERIALIZED ----- //
 M.AutoInit();
 
+// ----- INITIALISE MATERIALIZED GALLERY ----- //
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.materialboxed');
+	var instances = M.Materialbox.init(elems);
+});
+
+//----- INITIALISE MATERIALIZE CAROUSEL ----- //
 document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.carousel');
 	console.log(elems);
@@ -48,10 +68,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	  });
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(elems, options);
-  });
 
 
   
