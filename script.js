c934f1d0547246598f7b19f82d8fe657900858e6
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2019-05-03 00:41:51 +0100

    temp site

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("click", () =>{
+    document.querySelector('.msg2').style.display = "block";
+    document.querySelector('.tap-message').style.display = "none";
+    document.querySelector('.msg1').style.display = "none";
+})
